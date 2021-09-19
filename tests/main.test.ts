5ee514e671605839ee350051b6d5b71ac1085c60
lucaslah <47047328+Lucaslah@users.noreply.github.com>
2021-09-19 14:59:56 +1200

    Error Fixes

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,31 +2,31 @@ import logger from "../dist/index";
 import { expect } from '@jest/globals';
 
 describe("Log Text", () => {
-    test("Log Text should be correct, done => {
+    test("Log Text should be correct", done => {
         expect(logger.log("Hello World")).toBe("[LOG] Hello World");
     });
 });
 
-describe("Info Text", (): any => {
-    test("Info Text should be correct, done => {
+describe("Info Text", () => {
+    test("Info Text should be correct", done => {
         expect(logger.info("Hello World")).toBe("[INFO] Hello World");
     });
 });
 
-describe("Error Text", (): any => {
-    test("Error Text should be correct, done => {
+describe("Error Text", () => {
+    test("Error Text should be correct", done => {
         expect(logger.error("Hello World")).toBe("[ERROR] Hello World");
     });
 });
 
-describe("Warn Text", (): any => {
-    test("Warn Text should be correct, done => {
+describe("Warn Text", () => {
+    test("Warn Text should be correct", done => {
         expect(logger.warn("Hello World")).toBe("[WARN] Hello World");
     });
 });
 
-describe("Debug Text", (): any => {
-    test("Debug Text should be correct, done => {
+describe("Debug Text", () => {
+    test("Debug Text should be correct", done => {
         expect(logger.debug("Hello World")).toBe("[DEBUG] Hello World");
     });
 });
