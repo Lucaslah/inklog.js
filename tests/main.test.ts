606bfdb135150b3f7fc351d8323ec73dd9afd712
lucaslah <47047328+Lucaslah@users.noreply.github.com>
2021-09-19 14:53:24 +1200

    any hack for now

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -7,25 +7,25 @@ describe("Log Text", () => {
     });
 });
 
-describe("Info Text", () => {
+describe("Info Text", (): any => {
     test("Info Text should be correct, done => {
         expect(logger.info("Hello World")).toBe("[INFO] Hello World");
     });
 });
 
-describe("Error Text", () => {
+describe("Error Text", (): any => {
     test("Error Text should be correct, done => {
         expect(logger.error("Hello World")).toBe("[ERROR] Hello World");
     });
 });
 
-describe("Warn Text", () => {
+describe("Warn Text", (): any => {
     test("Warn Text should be correct, done => {
         expect(logger.warn("Hello World")).toBe("[WARN] Hello World");
     });
 });
 
-describe("Debug Text", () => {
+describe("Debug Text", (): any => {
     test("Debug Text should be correct, done => {
         expect(logger.debug("Hello World")).toBe("[DEBUG] Hello World");
     });
